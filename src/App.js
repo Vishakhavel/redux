cfa0d76bb820e88ad570ed6e33528c7024c92388
vichu <vichu2grand@gmail.com>
2022-10-05 13:56:19 +0530

    finished counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css'
 import { Fragment } from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+
 function App() {
+  const counter = useSelector((state) => state.counter)
+  const dispatch = useDispatch()
+
+  const incrementCounter = () => {
+    dispatch({ type: 'INC' })
+  }
+  const decrementCounter = () => {
+    dispatch({ type: 'DEC' })
+  }
   return (
     <Fragment>
       <h1>Counter App</h1>
+      <h2>{counter}</h2>
+      <button onClick={incrementCounter}>+</button>
+      <button onClick={decrementCounter}>-</button>
     </Fragment>
   )
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,16 @@
 import { createStore } from 'redux'
 
-const reducerFn = (state = { counter: 0 }, action) => {}
+const reducerFn = (state = { counter: 0 }, action) => {
+  // sync functions only.
+  // not mutate the original state, rather modify and return a copy of the original state.
+
+  if (action.type == 'INC') {
+    return { counter: state.counter + 1 }
+  } else if (action.type === 'DEC') {
+    return { counter: state.counter - 1 }
+  }
+  return state
+}
 const store = createStore(reducerFn)
 
 export default store
